a9d66d60410b78287326c863b821028e714a318b
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2025-04-27 16:17:18 +0200

    up

--- config/config.js
+++ config/config.js
@@ -10,6 +10,7 @@ export const DB_USER = process.env.DB_USER || "jaimenebot";
 export const DB_PASS = process.env.DB_PASS || "1234";
 export const DB_CLUSTER = process.env.DB_CLUSTER || "cei-practicas.i5jd4.mongodb.net";
 export const DATABASE= process.env.DATABASE || "base_de_datos";
+export const BACKEND_URL= process.env.BACKEND_URL || "https://backend-proyecto-fs-jaime-nebot.vercel.app/"
 
 export const JWT_SECRET = process.env.JWT_SECRET || "clave-super-secreta"
 
