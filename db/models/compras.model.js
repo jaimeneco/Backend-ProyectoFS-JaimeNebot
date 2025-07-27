b13c9ac0d6ec3d4654eee2dc441c197a452d4d4f
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2025-07-27 21:29:33 +0200

    up

--- db/models/compras.model.js
+++ db/models/compras.model.js
@@ -1,25 +1,25 @@
 // --------------------------------
 // NO ESTÁ IMPLEMENTADO EN LA WEB
 // --------------------------------
-import mongoose from 'mongoose'
+// import mongoose from 'mongoose'
 
-const options = {
-    collection: 'compras', // Nombre de la colección en mongoDB
-    strict: true,
-    collation: {
-        locale: "es",
-        strength: 1
-    }
-}
+// const options = {
+//     collection: 'compras', // Nombre de la colección en mongoDB
+//     strict: true,
+//     collation: {
+//         locale: "es",
+//         strength: 1
+//     }
+// }
 
-const comprasSchema = new mongoose.Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' },
-    products: [{
-        product: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto' },
-        quantity: Number,
-        priceAtPurchase: Number // por si el precio cambia con el tiempo
-    }],
-    createdAt: { type: Date, default: Date.now },
-}, options);
+// const comprasSchema = new mongoose.Schema({
+//     user: { type: mongoose.Schema.Types.ObjectId, ref: 'Usuario' },
+//     products: [{
+//         product: { type: mongoose.Schema.Types.ObjectId, ref: 'Producto' },
+//         quantity: Number,
+//         priceAtPurchase: Number // por si el precio cambia con el tiempo
+//     }],
+//     createdAt: { type: Date, default: Date.now },
+// }, options);
 
-export const Compra = mongoose.model('Compra', comprasSchema);
+// export const Compra = mongoose.model('Compra', comprasSchema);
