5011ee2487bb48d41dae29cad5bb18051f1c8d94
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2025-07-27 13:04:43 +0200

    up

--- routes/role.routes.js
+++ routes/role.routes.js
@@ -7,7 +7,7 @@ const router = Router();
 //Es una ruta protegida para acceder solo si se es admin
 
 router.post('/admin-only', authMiddleWare, isAdmin, (req, res)=>{
-    res.status(200).json({msg:'Bienvenido, amdin'})
+    res.status(200).json({msg:'Bienvenido, admin'})
 })
 
 export default router;
